b3ff93902ccdf2d789b8e5a8f76f1c48b6caded1
mds06f <madhurimadas408@gmail.com>
2026-07-18 11:35:44 +0530

    Implemented Custom CSS Injector

--- client/src/widgets/index.js
+++ client/src/widgets/index.js
@@ -23,7 +23,8 @@ export const widgetRegistry = {
       backgroundColor: 'transparent',
       backgroundImageUrl: '',
       borderColor: 'transparent',
-      borderWidth: '0px'
+      borderWidth: '0px',
+      customCSS: ''
     }
   },
   quote: {
@@ -42,7 +43,8 @@ export const widgetRegistry = {
       gradientName: 'royal',
       backgroundImageUrl: '',
       borderRadius: '12px',
-      showAuthor: true
+      showAuthor: true,
+      customCSS: ''
     }
   },
   weather: {
@@ -59,7 +61,8 @@ export const widgetRegistry = {
       backgroundStyle: 'gradient',
       gradientName: 'sunset',
       backgroundImageUrl: '',
-      borderRadius: '12px'
+      borderRadius: '12px',
+      customCSS: ''
     }
   }
 };
